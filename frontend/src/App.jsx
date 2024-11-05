20a987a894d79be27a1d671edbcfacc9cc5d1591
unknown <malik.rizwan177@yahoo.com>
2024-11-05 23:48:39 +0500

    First upload with frontend of home page completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const Home = lazy(() => import("./pages/Home"));
+const Collection = lazy(() => import("./pages/Collection"));
+const About = lazy(() => import("./pages/About"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Login = lazy(() => import("./pages/Login"));
+const Order = lazy(() => import("./pages/Order"));
+const PlaceOrder = lazy(() => import("./pages/PlaceOrder"));
+const Product = lazy(() => import("./pages/Product"));
+const Contact = lazy(() => import("./pages/Contact"))
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Suspense>
+      <Footer/>
+    </div>
+  );
+};
+
+export default App;
