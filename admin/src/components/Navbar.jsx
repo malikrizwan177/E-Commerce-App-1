f85ab74460be807cd1d9410771b9b7b6a1fe4514
unknown <malik.rizwan177@yahoo.com>
2024-11-10 13:37:09 +0500

    admin file setup

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from "../assets/assets"
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-2 px-[4%]">
+        <img src={assets.logo} alt="logo" className="w-[max(10%,80px)]"/>
+        <button className="bg-gray-600 text-white px-5 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">Logout</button>
+    </nav>
+  )
+}
+
+export default Navbar
