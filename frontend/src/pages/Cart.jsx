8797154680e822544514168b2a8afb02acaa4808
unknown <malik.rizwan177@yahoo.com>
2024-12-09 16:46:33 +0500

    another import error removed in cart.jsx

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../context/ShopContext"
 import Title from "../components/Title"
 import { assets } from "../assets/frontend_assets/assets"
-import CartTotal from "../components/cartTotal"
+import CartTotal from "../components/CartTotal"
 
 const Cart = () => {
 
